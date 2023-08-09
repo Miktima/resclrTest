0f1efce1c2bd7b8b07b6673ad6039b5b12a9e9ea
Тимофеев Михаил Константинович <m.timofeev@ria.ru>
2023-08-09 15:57:39 +0300

    add test with image direct

--- resclrTest.go
+++ resclrTest.go
@@ -27,18 +27,28 @@ func checkStatus(url string) {
 func main() {
 	// initial values
 	type initPar struct {
-		URLPage string
-		CDN     string
+		URLPage   string
+		CDN       string
+		FlagImage int
 	}
 	var readFile string
 	var ipar initPar
 	fmt.Printf("Read from configuration file (conf.json)? (Y/n)")
 	fmt.Scanln(&readFile)
 	if readFile == "n" || readFile == "N" {
-		fmt.Printf("Page URL to check jpeg images: ")
-		fmt.Scanln(&ipar.URLPage)
-		fmt.Printf("CDN on the page: ")
-		fmt.Scanln(&ipar.CDN)
+		fmt.Printf("Test image (1) of page (0)?")
+		fmt.Scanln(&ipar.FlagImage)
+		if ipar.FlagImage == 0 {
+			fmt.Printf("Page URL to check jpeg images: ")
+			fmt.Scanln(&ipar.URLPage)
+			fmt.Printf("CDN on the page: ")
+			fmt.Scanln(&ipar.CDN)
+		} else if ipar.FlagImage == 1 {
+			fmt.Printf("URL to image: ")
+			fmt.Scanln(&ipar.URLPage)
+		} else {
+			fmt.Println("ERROR: Unknown option: ", ipar.FlagImage)
+		}
 	} else {
 		// Open our jsonFile
 		byteValue, err := os.ReadFile("conf.json")
@@ -51,30 +61,43 @@ func main() {
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println("Page URL to test jpeg images: ", ipar.URLPage)
-		fmt.Println("CDN on the page: ", ipar.CDN)
+		fmt.Println("FlagImage: ", ipar.FlagImage)
+		if ipar.FlagImage == 0 {
+			fmt.Println("Page URL to test jpeg images: ", ipar.URLPage)
+			fmt.Println("CDN on the page: ", ipar.CDN)
+		} else if ipar.FlagImage == 1 {
+			fmt.Println("URL to image: ", ipar.URLPage)
+		} else {
+			fmt.Println("ERROR: Unknown option: ", ipar.FlagImage)
+		}
 	}
-	// Send an HTTP GET request to the urlPage web page
-	resp, err := http.Get(ipar.URLPage)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	defer resp.Body.Close()
-	// find all matched links
-	re := regexp.MustCompile(`(https://` + ipar.CDN + `[\w/.:]*(webp|jpg))`)
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	content := string(body)
-	links := re.FindAllString(content, -1)
-	// loops through the links slice to check get status
-	// increase number of requests by 10
-	for i := 0; i < 10; i++ {
-		for _, l := range links {
-			go checkStatus(l)
+	if ipar.FlagImage == 0 {
+		// Send an HTTP GET request to the urlPage web page
+		resp, err := http.Get(ipar.URLPage)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		defer resp.Body.Close()
+		// find all matched links
+		re := regexp.MustCompile(`(https://` + ipar.CDN + `[\w/.:]*(webp|jpg))`)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		content := string(body)
+		links := re.FindAllString(content, -1)
+		// loops through the links slice to check get status
+		// increase number of requests by 10
+		for i := 0; i < 10; i++ {
+			for _, l := range links {
+				go checkStatus(l)
+			}
+		}
+	} else if ipar.FlagImage == 1 {
+		for i := 0; i < 1500; i++ {
+			go checkStatus(ipar.URLPage)
 		}
 	}
 	var input string
